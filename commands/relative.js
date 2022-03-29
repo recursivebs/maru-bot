9765ff6e160a34a8a73f3cee31c639c6f2cc9633
recur <recur@DESKTOP-25JSD1E>
2022-03-28 18:18:27 -0700

    Fix

--- commands/relative.js
+++ commands/relative.js
@@ -22,25 +22,40 @@ module.exports = {
         const playerInfoEndpoint = `https://marubot.bluecurse.com/report/player-medals/player-id/${player_id}`;
 
         let playerIndex = 0;
+		playerInfo = {};
 
         fetch(playerInfoEndpoint)
 			.then((response) => response.json())
 			.then(data => {
-				playerIndex = data["medal_rank"];
-				if (playerIndex < 6) {
-					console.log(playerIndex);
-					playerIndex = 6;
-				}
+
+				playerInfo = data;
+
 				const reportEndpoint = "https://marubot.bluecurse.com/report/player-medals";
 				return fetch(reportEndpoint)
 					.then((response) => response.json())
 					.then(data => {
-						const topTen = data.slice(playerIndex - 6, playerIndex + 4);
+
+						// Need to filter out all other players with the same rank
+						// as the current player, we don't care about them
+						let filteredData = data.filter(player => {
+							if (player.medal_rank === playerInfo.medal_rank) {
+								return player.player_id === playerInfo.player_id;
+							}
+							return true;
+						});
+
+						playerIndex = filteredData.map(player => player.player_id).indexOf(playerInfo.player_id);
+						if (playerIndex < 6) {
+							playerIndex = 6;
+						}
+						console.log(playerIndex);
+
+						const topTen = filteredData.slice(playerIndex - 6, playerIndex + 4);
 						let embed = new MessageEmbed()
 							.setColor("#EFFF00")
 							.setTitle("Relative Leaderboard")
 						let message = ""
-						topTen.forEach(x => message = ((x.medal_rank === playerIndex)? '**' : '') + `${message}${x.medal_rank} - ${x.player_name} - ${x.medals}` + ((x.medal_rank === playerIndex)? '**' : '') + `\n`)
+						topTen.forEach(x => message = ((x.player_id === playerInfo.player_id)? '**' : '') + `${message}${x.medal_rank} - ${x.player_name} - ${x.medals}` + ((x.player_id === playerInfo.player_id)? '**' : '') + `\n`)
 						embed.setDescription(message);
 						interaction.editReply({embeds: [embed]});
 					});
